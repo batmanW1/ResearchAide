58634d9641811e0472bae2ea38a35245c96c2e1b
juntao <juntao@seas.upenn.edu>
2014-10-07 11:02:40 -0400

    Add functional log-in screen

---
+++ src/edu/upenn/med/researchaide/MainActivity.java
@@ -0,0 +1,55 @@
+package edu.upenn.med.researchaide;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    /**
+     * Called when the user clicks the sign-up button
+     * @param view
+     */
+    public void signUp(View view) {
+    	Intent intent = new Intent(this, SignUpActivity.class);
+    	startActivity(intent);
+    }
+    
+    /**
+     * Calledwhen th user clicks the log-in button
+     * @param view
+     */
+    public void logIn(View view) {
+    	
+    }
+}
